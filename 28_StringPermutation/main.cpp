9e7d7d330b45adbc5b7581eaf876e6c95fe4088f
yoon <yinxiangkai520@163.com>
2021-12-22 20:53:47 +0800

    [字符串的排列]

---
+++ 28_StringPermutation/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+void Permutation(char *pStr, char *pBegin) {
+    if (*pBegin == '\0') {
+        printf("%s\n",pStr);
+    } else {
+        for (char * pCh = pBegin;*pCh != '\0';++ pCh) {
+            char temp = *pCh;
+            *pCh = *pBegin;
+            *pBegin = temp;
+            Permutation(pStr,pBegin + 1);
+
+            temp = *pCh;
+            *pCh = *pBegin;
+            *pBegin = temp;
+        }
+    }
+}
+
+void Permutation(char *pStr) {
+    if (pStr == nullptr) return;
+    Permutation(pStr,pStr);
+}
+
+void test() {
+    char cArray[] ="abc";
+    Permutation(cArray);
+
+    char cArray1[] ="a";
+    Permutation(cArray1);
+}
+
+int main()
+{
+    test();
+    return 0;
+}
